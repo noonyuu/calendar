2a8e7136793fd6ecd0c780da9e82f941e49faa84
shimizu <noon3m.op.sh@gmail.com>
2024-02-18 00:27:16 +0900

    [add]カレンダーを表示させるようにした

--- calendar/src/index.tsx
+++ calendar/src/index.tsx
@@ -1,16 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { Header } from "./components/Header";
+import { Calendar } from "./components/calendarComponents/Calendar";
+import { ContextWrapper } from "./context/ContextWrapper";
+// import Footer from "./components/footer";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Header />
+    <ContextWrapper>
+      <Calendar />
+    </ContextWrapper>
+    {/* <Footer /> */}
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
