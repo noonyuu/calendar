e8b7bea2c7b6dd7e822838369fbb230d1e3e7b94
shimizu <noon3m.op.sh@gmail.com>
2024-02-20 23:32:43 +0900

    [add]global context

--- calendar/src/context/GlobalContext.tsx
+++ calendar/src/context/GlobalContext.tsx
@@ -1,6 +1,15 @@
+import { Dayjs } from "dayjs";
 import React from "react";
 
 export const GlobalContext = React.createContext({
   monthIndex: 0,
   setMonthIndex: (index: number) => {},
+  daySelected: null as Dayjs | null,
+  setDaySelected: (day: Dayjs | null) => {},
+  showEventModal: false,
+  setShowEventModal: (show: boolean) => {},
+  dispatchCalEvent: ({ type, payload }: { type: string; payload: any }) => {},
+  savedEvents: [] as any[],
+  selectedEvent: null as any,
+  setSelectedEvent: (event: any) => {},
 });
